42136d630629cf23bc328139dfcb9e27a8010628
DarkNormal <mark.lordan@me.com>
2015-09-17 19:30:08 +0100

    Polymorphism exercise start

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vertex v = new Vertex(10, 5);
+            Console.WriteLine("X,Y Coordinates of first vertex: {0},{1}", v.GetXCoord, v.GetYCoord);
+            Console.ReadLine();
+        }
+    }
+    class Vertex
+    {
+        private int xCoord, yCoord;
+        public int GetXCoord
+        {
+            get
+            {
+                return xCoord;
+            }
+        }
+        public int GetYCoord
+        {
+            get
+            {
+                return yCoord;
+            }
+        }
+        public Vertex(int xCoord, int yCoord)
+        {
+            this.xCoord = xCoord;
+            this.yCoord = yCoord;
+
+        }
+    }
+
+    class Shape
+    {
+        private string color;
+       
+    }
+}
