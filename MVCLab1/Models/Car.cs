4e5775bc6e53bfa0ca52ef82466052d17564aec8
Mark Lordan <mark.lordan@me.com>
2015-11-05 11:25:44 +0000

    MVC exercise with basic tasks

---
+++ MVCLab1/Models/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCLab1.Models
+{
+    public enum EngineType
+    {
+        Petrol,Diesel,Hybrid
+    }
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public double EngineSize { get; set; }
+        public EngineType Enginetype { get; set; }
+
+        public Car(string make, string model, double engineSize, EngineType engineType)
+        {
+            Make = make;
+            Model = model;
+            EngineSize = engineSize;
+            Enginetype = engineType;
+        }
+    }
+}
---
+++ MVCLab1/Models/TextMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCLab1.Models
+{
+    public class TextMessage
+    {
+        public int TextMessageId { get; set; }
+        [Required]
+        [MaxLength(10, ErrorMessage = "Phone Number must be 10 digits long"), MinLength(10, ErrorMessage = "Phone Number must be 10 digits long")]
+        [DataType(DataType.PhoneNumber, ErrorMessage ="Must be a phone number")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [MaxLength(140, ErrorMessage = "Max length of message is 140 characters")]
+        public string Content { get; set; }
+    }
+}
